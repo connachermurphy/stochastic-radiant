31844dbcb661c9a6488eceecffa76437fb73d59a
Connacher Murphy <connacher.murphy@gmail.com>
2025-08-04 19:43:29 -0700

    Update readme and single forecast example

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# stochastic-radiant
-LLM-generated forecasts
+# stochastic-radiant: LLM-generated forecasts
+I implement a barebones tool for LLM-generated forecasts. The tool only supports the Anthropic API at present.
+
+## Example usage
+See `examples/single_forecast.py` for sample usage on a single forecasting question. The script requires a `.env` file in the project root:
+```
+ANTHROPIC_API_KEY={your_anthropic_api_key}
+```
+
+## Contributions
+Please contact me if you're interested in contributing. These tools are being actively developed for use in a submission to [ForecastBench](https://www.forecastbench.org/).
--- examples/single_forecast.py
+++ examples/single_forecast.py
@@ -4,17 +4,25 @@ import dotenv
 
 from stochastic_radiant.forecaster import Forecaster
 
+# Load the API key from the .env file
 dotenv.load_dotenv()
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
+
+# Specify the model to use for forecasting
 ANTHROPIC_MODEL = "claude-sonnet-4-20250514"
 
+# Initialize the forecaster from the Forecaster class
+# Pass in the model and the API key
 forecaster = Forecaster(model=ANTHROPIC_MODEL, api_key=ANTHROPIC_API_KEY)
 
+# Create a question to forecast
 question = """
 I am about to flip a coin. What is the probability that it will land on heads?
 """
 
+# The forecast method will return a tuple of the forecast and the rationale
 forecast, rationale = forecaster.forecast(question)
 
+# Print the forecast and the rationale
 print(f"Forecast: {forecast}")
 print(f"Rationale: {rationale}")
